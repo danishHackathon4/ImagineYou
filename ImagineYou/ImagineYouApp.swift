7704f7b8734a703c8c0a249e449cdf3060b8ec1d
danishgct <68178862+danishgct@users.noreply.github.com>
2023-09-22 10:38:34 -0500

    First Commit

---
+++ ImagineYou/ImagineYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagineYouApp.swift
+//  ImagineYou
+//
+//  Created by Danish Phiroz on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ImagineYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
